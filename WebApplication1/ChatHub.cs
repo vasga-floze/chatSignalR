568ff800cdfb2b42d6b5eabd0d47ea09bf0c2926
vasga-floze <gabriela.floze@gmail.com>
2022-06-19 19:36:06 -0600

    updates some packages, chat is already working

---
+++ WebApplication1/ChatHub.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1
+{
+    public class ChatHub : Hub
+    {
+        //fuction to send the messages of our chat
+        public void Send(string name, string message)
+        {
+            //specify the method for send to all clients
+            Clients.All.SendChat(name, message);
+        }
+    }
+}
